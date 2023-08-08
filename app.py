38b1f1fef22178fc91c75f50001ce2a8705e0d7a
poqop721 <poqop721@naver.com>
2023-08-09 03:41:03 +0900

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import *
 from flask_bcrypt import Bcrypt
  
 app = Flask(__name__)
-client = MongoClient('mongodb://test:test@3.36.71.132', 27017)
+client = MongoClient('mongodb://seungtae:jeon8175@13.125.153.232', 27017)
 dblog = client.jungle_food_feed #db명
 
 app.config.update(
@@ -49,6 +49,27 @@ def login():
     else :
         return jsonify({'result': 'success', 'id':id,'msg':'로그인 되었습니다!', 'access_token' : create_access_token(identity = id,
 											expires_delta = False)})
+    
+#like feed
+@app.route('/api/like', methods=['POST'])
+def like_feed():
+   #클라이언트가 전달한 name_give를 name_receive 변수에 넣기
+   print(1)
+   name_receive = request.form['name_give']
+   print(1)
+   feed_data = {
+      'place_name' : name_receive
+   }
+   #MongoDB에 데이터 넣기
+   dblog.feeds.insert_one(feed_data)
+#    #feeds 목록에서 find_one 으로 name이 name_receive와 일치하는 feed를 찾는다
+#    feed = dblog.feeds.find_one({'name':name_receive})
+#    #feed의 like에 1을 더해준 new_like 변수를 만든다.
+#    new_like = feed['like'] + 1
+#    #feeds 목록에서 name이 name_receive인 문서의 like를 new_like로 변경
+#    dblog.feeds.update_one({'name':name_receive}, {'$set':{'like': new_like}})
+#    #성공하면 success 반환
+   return jsonify({'result':'success'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000,debug=True)
